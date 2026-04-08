316af9bcdb6a146a86020bdf93d569d3687713c5
aokutu <muleli.haddad@gmail.com>
2026-04-08 19:00:09 +0300

    DRIVER MAP API

--- main.go
+++ main.go
@@ -6,25 +6,43 @@ import (
 	"net/http"
 )
 
+type data struct {
+	Street string  // exported so template can see it
+}
+
 func main() {
-	http.HandleFunc("/F-MAP", FrontMapHandler)
+	var details data
+
+	// Pass details (or a pointer to it) into the handler
+	// by wrapping it in a closure
+	http.HandleFunc("/F-MAP", func(w http.ResponseWriter, r *http.Request) {
+		FrontMapHandler(w, r, &details)
+	})
+
+	http.HandleFunc("/BOOKPARKING", Bookparking)
 
 	fmt.Println("Server running on http://localhost:8080")
 	http.ListenAndServe(":8080", nil)
 }
 
-func FrontMapHandler(w http.ResponseWriter, r *http.Request) {
-	// Make sure the file exists and is reachable
+func FrontMapHandler(w http.ResponseWriter, r *http.Request, d *data) {
+	// d is *data, so you can modify it
+	d.Street = "KIMATHI STREET"
+
 	tmpl, err := template.ParseFiles("maps/map.html")
 	if err != nil {
 		http.Error(w, "Template error: "+err.Error(), http.StatusInternalServerError)
 		return
 	}
 
-	data := "DATA"
-	err = tmpl.Execute(w, data)
+	err = tmpl.Execute(w, d)
 	if err != nil {
 		http.Error(w, "Execute error: "+err.Error(), http.StatusInternalServerError)
 		return
 	}
+}
+
+func Bookparking(w http.ResponseWriter, r *http.Request){
+
+fmt.Fprintf(w, "Hello, this is your Go HTTP server!")
 }
